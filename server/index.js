34872396c7f476a884735c87cf716abf50981844
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2021-03-15 13:04:08 +0100

    prepare app for heroku

--- server/index.js
+++ server/index.js
@@ -136,7 +136,7 @@ function updateResultsInTable(table) {
   } 
 }
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
